14d7a60ad04860a1a95d660742632a60a3b17989
capricornus <aleksandrzhupanov@gmail.com>
2016-08-22 18:33:47 +0300

    init

---
+++ src/main/java/com/jubaka/websockets/DeviceWebSocketServer.java
@@ -0,0 +1,68 @@
+package com.jubaka.websockets;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.StringReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by root on 18.08.16.
+ */
+@Named
+@javax.enterprise.context.ApplicationScoped
+@ServerEndpoint("/actions")
+public class DeviceWebSocketServer {
+
+    @Inject
+    private DeviceSessionHandler sessionHandler;
+
+    @OnOpen
+    public void open(Session session) {
+        sessionHandler.addSession(session);
+    }
+
+    @OnMessage
+    public void handleMessage(String message, Session session) {
+
+        try (JsonReader reader = Json.createReader(new StringReader(message))) {
+            JsonObject jsonMessage = reader.readObject();
+
+            if ("add".equals(jsonMessage.getString("action"))) {
+                Device device = new Device();
+                device.setName(jsonMessage.getString("name"));
+                device.setDescription(jsonMessage.getString("description"));
+                device.setType(jsonMessage.getString("type"));
+                device.setStatus("Off");
+                sessionHandler.addDevice(device);
+            }
+
+            if ("remove".equals(jsonMessage.getString("action"))) {
+                int id = (int) jsonMessage.getInt("id");
+                sessionHandler.removeDevice(id);
+            }
+
+            if ("toggle".equals(jsonMessage.getString("action"))) {
+                int id = (int) jsonMessage.getInt("id");
+                sessionHandler.toggleDevice(id);
+            }
+        }
+    }
+
+    @OnClose
+    public void close(Session session) {
+        sessionHandler.removeSession(session);
+    }
+
+    @OnError
+    public void onError(Throwable error) {
+        Logger.getLogger(DeviceWebSocketServer.class.getName()).log(Level.SEVERE, null, error);
+    }
+
+}
